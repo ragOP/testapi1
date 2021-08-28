d13601529b7a1d3264b1ec1ce07f1732d4b68f58
ragOP <84832906+ragOP@users.noreply.github.com>
2021-08-28 13:45:37 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+ 
+
+  const [user,setUser]=useState([]);
+  const getUser = async()=>
+  {
+    const response=await fetch('https://mocki.io/v1/b6244d84-5737-4dfa-ae74-9cb1fb0493a1');
+    //console.log(response)
+     setUser(await response.json());
+    
+  }
+
+   useEffect(() => {
+    getUser();
+    
+   
+  }, [])
+  const display = Object.entries(user).map(( item) => {
+    return (
+      <div className="my-posts">
+        <h6 key={item[1]}>
+         {item}
+        </h6>
+      </div>
+      );
+    });
+
+
+
+
+
+    // {
+
+    //   user.map((anything)=>{
+    //     return <div key={user.id}>
+    //       <h2>{anything.title}</h2>
+    //       <h4>{anything.url}</h4>
+    //     </div>
+    //   })
+    // } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<>
+
+    <div className="App" key={user.id}>
+
+     <h1> {display}</h1>
     </div>
+    </>
   );
+  
 }
 
 export default App;
